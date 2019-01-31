48a8444004d591406b3a6165261413b4cbcddbd7
IntroCDC <brunocoelhosabino@gmail.com>
2019-01-31 06:10:24 -0300

    Uploading to GitHub

--- src/br/com/introcdc/mapmeelv1/MapListeners.java
+++ src/br/com/introcdc/mapmeelv1/MapListeners.java
@@ -1,33 +1,37 @@
 package br.com.introcdc.mapmeelv1;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.PlayerBucketEmptyEvent;
-import org.bukkit.event.player.PlayerBucketFillEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.*;
 
 public class MapListeners implements Listener {
 
+    public static Location SPAWN = new Location(Bukkit.getWorlds().get(0), 83.0, 43.0, 177.0);
+
     @EventHandler
-    public void onBreak(final BlockBreakEvent event) {
+    public void onBreak(BlockBreakEvent event) {
         event.setCancelled(!event.getPlayer().getName().equalsIgnoreCase("Introo"));
     }
 
     @EventHandler
-    public void onEmpty(final PlayerBucketEmptyEvent event) {
+    public void onEmpty(PlayerBucketEmptyEvent event) {
         event.setCancelled(!event.getPlayer().getName().equalsIgnoreCase("Introo"));
     }
 
     @EventHandler
-    public void onFill(final PlayerBucketFillEvent event) {
+    public void onFill(PlayerBucketFillEvent event) {
         event.setCancelled(!event.getPlayer().getName().equalsIgnoreCase("Introo"));
     }
 
     @EventHandler
-    public void onInteract(final PlayerInteractEvent event) {
+    public void onInteract(PlayerInteractEvent event) {
         if (event.getClickedBlock() != null) {
             if (!event.getClickedBlock().getType().equals(Material.WOOD_BUTTON) && !event.getClickedBlock().getType().equals(Material.LEVER) && !event.getClickedBlock().getType().equals(Material.POWERED_RAIL)) {
                 event.setCancelled(!event.getPlayer().getName().equalsIgnoreCase("Introo"));
@@ -36,7 +40,26 @@ public class MapListeners implements Listener {
     }
 
     @EventHandler
-    public void onPlace(final BlockPlaceEvent event) {
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        if (event.getPlayer().getLocation().getY() < 0) {
+            event.setTo(SPAWN);
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        event.getPlayer().teleport(SPAWN);
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
         event.setCancelled(!event.getPlayer().getName().equalsIgnoreCase("Introo"));
     }
 
--- src/br/com/introcdc/mapmeelv1/MapMain.java
+++ src/br/com/introcdc/mapmeelv1/MapMain.java
@@ -1,10 +1,19 @@
 package br.com.introcdc.mapmeelv1;
 
+import br.com.introcdc.mapmeelv1.commands.CommandComplete;
+import com.google.common.base.Preconditions;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import net.minecraft.server.v1_8_R3.PlayerConnection;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import br.com.introcdc.mapmeelv1.commands.CommandComplete;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class MapMain extends JavaPlugin {
 
@@ -17,8 +26,37 @@ public class MapMain extends JavaPlugin {
     @Override
     public void onEnable() {
         MapMain.plugin = this;
-        this.getCommand("mapmeelv1complete").setExecutor(new CommandComplete());
+        getCommand("mapmeelv1complete").setExecutor(new CommandComplete());
         Bukkit.getPluginManager().registerEvents(new MapListeners(), MapMain.getPlugin());
+        Bukkit.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+    }
+
+    public static void sendPlayer(Player player, String server) {
+        sendPluginMessage(player, "Connect", server);
+    }
+
+    public static void sendPluginMessage(Player player, String... messages) {
+        ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+        DataOutputStream output = new DataOutputStream(byteArray);
+        try {
+            for (String message : messages) {
+                output.writeUTF(message);
+            }
+        } catch (IOException ignored) {
+        }
+        player.sendPluginMessage(getPlugin(), "BungeeCord", byteArray.toByteArray());
+    }
+
+    public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int duration, int fadeOut) {
+        Preconditions.checkNotNull(title, "Title cannot be null");
+        Preconditions.checkNotNull(subtitle, "Subtitle cannot be null");
+        PacketPlayOutTitle timingsPacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, fadeIn, duration, fadeOut);
+        PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + title + "\"}"));
+        PacketPlayOutTitle subtitlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + subtitle + "\"}"));
+        PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().playerConnection;
+        playerConnection.sendPacket(timingsPacket);
+        playerConnection.sendPacket(titlePacket);
+        playerConnection.sendPacket(subtitlePacket);
     }
 
 }
